8149609d345685e14902c7e932981b06b5c7066a
AciernoSimone <acierno.simone@iisgalvanimi.edu.it>
2020-03-31 09:56:20 +0000

    finito

---
+++ maps/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { AgmCoreModule } from '@agm/core'; //importa questa libreria
+import { AppComponent } from './app.component';
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    // Imposta qui le tue api key
+     AgmCoreModule.forRoot({apiKey: 'AIzaSyBJ0CDwHc6nPa5vUQnMDtHv1uEawgLwtqw' }),
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
